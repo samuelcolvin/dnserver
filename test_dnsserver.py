86c75e0cc413435181bc4e706cd032b4a7008af0
Éloi Rivard <eloi.rivard@aquilenet.fr>
2022-09-14 17:42:53 +0200

    Added unit test
    - Added a DNSServer class in dnserver.py
    - Added a simple unit test
    - Added a GHA configuration

---
+++ test_dnsserver.py
@@ -0,0 +1,25 @@
+import pytest
+from dnserver import DNSServer
+import dns.resolver
+
+
+@pytest.fixture
+def dnsresolver():
+    port = 5053
+    with open("example_zones.txt") as fd:
+        zone = fd.read()
+
+    server = DNSServer(port, zone)
+    server.start()
+
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = ["127.0.0.1"]
+    resolver.port = port
+    yield resolver
+
+    server.stop()
+
+
+def test_dnsserver(dnsresolver):
+    answers = dnsresolver.resolve("example.com", "A")
+    assert answers[0].address == "1.2.3.4"
